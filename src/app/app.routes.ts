2c26195743d34d5fab7e0bfb29b32caec8976bf0
ebourgeo <devebourgeo@gmail.com>
2024-03-07 15:35:59 +0100

    :sparkles: routes + error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: ErrorComponent },
+];
