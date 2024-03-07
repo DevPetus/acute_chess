1d8dc36222099d277411c7a206de41ead0c68508
GuillaumeVern <guillaume.vern@outlook.com>
2024-03-07 16:25:01 +0100

    :sparkles: add board and menu components

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BoardComponent } from './board/board.component';
+import { MenuComponent} from './menu/menu.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [BoardComponent, MenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
