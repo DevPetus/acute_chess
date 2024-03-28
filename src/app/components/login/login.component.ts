b561fcd19b68c51301ea57b4f0f9896b6c94c791
ebourgeo <devebourgeo@gmail.com>
2024-03-28 10:44:41 +0100

    :sparkles: Login Page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,12 +2,27 @@ import { Component } from '@angular/core';
 import { RegisterComponent } from '../register/register.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input'
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RegisterComponent, MatButtonModule, MatIconModule],
+  imports: [RegisterComponent, MatButtonModule, MatIconModule,
+    MatFormFieldModule, MatCardModule, ReactiveFormsModule, MatInputModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  submit() {
+    console.log(this.form.value)
+  }
+}
