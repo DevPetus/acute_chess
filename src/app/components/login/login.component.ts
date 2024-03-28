087b14753d0c1bc37013f8927adbaba361b37f82
ebourgeo <devebourgeo@gmail.com>
2024-03-28 09:16:01 +0100

    :sparkles: auth init

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RegisterComponent } from '../register/register.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RegisterComponent, MatButtonModule, MatIconModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
