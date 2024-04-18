17eafd3145ba2f16a028277d8a7ee0df96096fa9
GuillaumeVern <guillaume.vern@outlook.com>
2024-04-18 11:39:06 +0200

    :sparkles: menu

--- src/app/components/home/menu/menu.component.ts
+++ src/app/components/home/menu/menu.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
